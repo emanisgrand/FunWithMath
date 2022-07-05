4ea58e1959569ac57ab31e4106d6715e29548e62
e-man <tenhitcombo@gmail.com>
2022-07-05 15:07:16 -0400

    setparent for graph points. variable instantiation

--- Assets/scripts/Graph.cs
+++ Assets/scripts/Graph.cs
@@ -6,22 +6,26 @@ public class Graph : MonoBehaviour
 {
     [SerializeField]
     Transform pointPrefab;
-    [SerializeField]
-    int resolution;
+
+    [SerializeField, Range(10,100)]
+    int resolution = 10;
+    
     void Awake()
     {
+        float step = 2f / resolution;
         Vector3 position = Vector3.zero;
-        var scale = Vector3.one / 5f;
+        var scale = Vector3.one * step;
 
         for (int i = 0; i < resolution; i++)
         {
             Transform point = Instantiate(pointPrefab);
             // f(x) = x
-            position.x = (i + 0.5f) / 5f - 1f;
+            position.x = (i + 0.5f) * step - 1f;
             position.y = position.x * position.x;
             
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform, false);
         }
     }
 }
